35043ed7f61325850e5fa7aad743cb0bd03275a9
Dhruv Parashar <dhruvparashar6@gmail.com>
2018-01-23 22:51:53 +0000

    add My component

---
+++ bookclub/src/All.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class MyComp extends Component{
+    
+    constructor(props){
+        super(props);
+        
+        this.state={
+            newbook:'',
+            addedbook:''
+        }
+    }
+    
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MyComp;
--- bookclub/src/App.js
+++ bookclub/src/App.js
@@ -3,10 +3,43 @@ import MyComp from './My.js'
 
 
 class App extends Component {
+    
+    constructor(props){
+        super(props);
+        
+        this.state={
+            newbook:'',
+            addedbook:''
+        }
+    }
+    
+    inputNewBook = (event) => {
+        this.setState({
+            newbook: event.target.value
+        });
+    }
+    
+    addNewBook = (event) => {
+        event.preventDefault()
+        this.setState({
+            addedbook: this.state.newbook
+        });
+    }
+    
+    alldisplay = (event) => {
+        console.log('allBooks');
+    }
+    
+    
     render() {
         return (
             <div className="App">
-                <MyComp />
+                <MyComp newbook={this.state.newbook}
+                addedbook={this.state.addedbook}
+                inputNewBook={this.inputNewBook}
+                addNewBook={this.addNewBook}
+                alldisplay={this.alldisplay}
+                />
             </div>
         );
     }
