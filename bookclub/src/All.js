cd7547394d1c10001f746867af26e09524324483
Dhruv Parashar <dhruvparashar6@gmail.com>
2018-01-24 17:30:53 +0000

    show thumbnails

--- bookclub/src/All.js
+++ bookclub/src/All.js
@@ -1,15 +1,64 @@
 import React, {Component} from 'react';
+import "isomorphic-fetch";
 
 class AllComp extends Component{
     
     constructor(props){
         super(props);
+        this.state={
+            link:''
+        }
+    }
+
+    /*
+    componentDidMount(){
+    
+        fetch('/myapi',{
+            method: 'POST',
+            body: JSON.stringify({
+                myquery: this.props.addedbook
+            }),
+            headers: {"Content-Type": "application/json"}
+        })
+            .then( (response) => {return response.json(); })
+            .then( (data) => {
+            	console.log(data)
+            })
+    }
+    
+    
+    */
+    
+    seeAll = () => {
+        
+        fetch('/myapi',{
+            method: 'POST',
+            body: JSON.stringify({
+                myquery: this.props.addedbook
+            }),
+            headers: {"Content-Type": "application/json"}
+        })
+            .then( (response) => {return response.json(); })
+            .then( (data) => {
+            	console.log(data.insert[0].thumbnail)
+            	this.setState({
+            	    link:data.insert[0].thumbnail
+            	})
+            })
+        
     }
     
     render(){
+        
         return(
             <div>
+                <button onClick={this.seeAll}>View added Books</button>
                 <h1>{this.props.addedbook}</h1>
+                <a href='#'> 
+                    <img src={this.state.link} alt="Book!"  /> 
+                </a>
+
+                
             </div>
         );
     }
--- index.js
+++ index.js
@@ -17,17 +17,20 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', function(req, res) {    
-    res.json({insert: "success"});
-    books.search('Professional JavaScript for Web Developers', function(error, results) {
+app.post('/myapi', function(req, res) {    
+    //res.json({insert: "success"});
+    
+    //console.log(req.body.myquery);
+    books.search(req.body.myquery, function(error, results) {
         if ( ! error ) {
-            console.log(results);
+            res.json({insert:results});
         } else {
             console.log(error);
         }
     });
+    
 });
 
-http.createServer(app).listen(8080, function(){
-    console.log("Example of app listning on port 8080");
+http.createServer(app).listen(8081, function(){
+    console.log("Example of app listning on port 8081");
 });
